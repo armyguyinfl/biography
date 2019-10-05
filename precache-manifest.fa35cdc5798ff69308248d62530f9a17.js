e065f2c15606112ddacca8a03f90f735b90fd14b
Daniel Kowalec <dkowalec@mdvip.com>
2019-10-05 17:58:47 -0400

    Bootstrap React Profile 1.0.0

---
+++ precache-manifest.fa35cdc5798ff69308248d62530f9a17.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49053fad98119eef6e5af74eff7078c8",
+    "url": "/biography/index.html"
+  },
+  {
+    "revision": "6ac39a2f3cd1df8a939d",
+    "url": "/biography/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1cbf0a9f6b29330133fd",
+    "url": "/biography/static/css/main.46605319.chunk.css"
+  },
+  {
+    "revision": "6ac39a2f3cd1df8a939d",
+    "url": "/biography/static/js/2.458d86f2.chunk.js"
+  },
+  {
+    "revision": "1cbf0a9f6b29330133fd",
+    "url": "/biography/static/js/main.0d472d92.chunk.js"
+  },
+  {
+    "revision": "312b02ab6732248e5d4e",
+    "url": "/biography/static/js/runtime~main.6f1af508.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/biography/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/biography/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/biography/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/biography/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "3a9e5f159cd395cb20dabcb4e2cd939e",
+    "url": "/biography/static/media/avatar-background-coding_75x400.3a9e5f15.jpeg"
+  },
+  {
+    "revision": "d286c5eb735252b2d4a7cca6048aa61b",
+    "url": "/biography/static/media/face-daniel.d286c5eb.jpeg"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/biography/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/biography/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/biography/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/biography/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/biography/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
